014d43830088f51017d6f437212a11bd18d1c3d0
Mario Hartleb <mhartleb83@yahoo.com>
2021-03-04 15:00:50 +0100

    commit_Mario

---
+++ mario_int.js
@@ -0,0 +1,16 @@
+//Intermediate 1
+
+// var capitalize = (function(){
+    
+//     var sentence =("i am a web developer");
+//     var upcase= (sentence.charAt(0).toUpperCase());}
+
+//     console.log (upcase);
+
+//intermediate 2
+
+function averageGrade  {
+
+    
+}
+
